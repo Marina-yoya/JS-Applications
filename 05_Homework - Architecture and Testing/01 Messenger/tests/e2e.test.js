817952828993e44c6194bc2d2c19bae5fda8d9ee
Marina Dimitrova <marina.dimitrova.yoya@gmail.com>
2021-09-23 15:55:30 +0300

    Solution added

---
+++ 05_Homework - Architecture and Testing/01 Messenger/tests/e2e.test.js
@@ -0,0 +1,61 @@
+const { expect } = require('chai');
+const { chromium } = require('playwright-chromium');
+
+const host = 'http://localhost:3000';
+
+let browser;
+let context;
+let page;
+
+// The current tests are running using the actual server and data in it, and the tests itself
+// will modified the data  until the server is restarted, so keep that in mind!
+
+describe('E2E tests', function () {
+    this.timeout(6000);
+
+    before(async () => {
+        // browser = await chromium.launch({ headless: false, slowMo: 500 });
+        browser = await chromium.launch();
+    });
+
+    after(async () => {
+        await browser.close();
+    });
+
+    beforeEach(async () => {
+        context = await browser.newContext();
+
+        page = await context.newPage();
+
+        await page.goto(host);
+    });
+
+    afterEach(async () => {
+        await page.close();
+        await context.close();
+    });
+
+    describe('Home', () => {
+        it('loading all messages', async () => {
+            await page.click('#refresh');
+            const messages = await page.$eval('#messages', (el) => el.value.split('\n'));
+
+            expect(messages[0]).to.eql('Spami: Hello, are you there?');
+            expect(messages[1]).to.eql('Garry: Yep, whats up :?');
+            expect(messages[2]).to.eql('Spami: How are you? Long time no see? :)');
+            expect(messages[3]).to.eql('George: Hello, guys! :))');
+            expect(messages[4]).to.eql('Spami: Hello, George nice to see you! :)))');
+        });
+
+        it('sending new message', async () => {
+            await page.fill('#author', 'Author');
+            await page.fill('#content', 'Message...');
+
+            await page.click('#submit');
+            await page.click('#refresh');
+
+            const messages = await page.$eval('#messages', (el) => el.value.split('\n'));
+            expect(messages[5]).to.eql('Author: Message...');
+        });
+    });
+});
